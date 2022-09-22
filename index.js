b01806a3c4313d3b565013b2a2bc37240cee9289
Kanan Amirov <amirovknn@gmail.com>
2022-09-22 19:40:25 +0400

    admin panel crud backend

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const multer = require('multer')
+var cors = require('cors')
+
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser')
+
+app.use(express.static(__dirname+'/public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors())
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({storage: storage})
+
+
+
+
+let idCounter = 3
+
+let data = [
+    {
+        id:1,
+        name:"emil",
+        surname:"hajizade"
+    },
+    {
+        id:2,
+        name:"kenan",
+        surname:"Amirov"
+    },
+    {
+        id:3,
+        name:"Abulfaz",
+        surname:"Sadiqov"
+    },
+]
+
+// app.post('/stats', upload.single('uploaded_file'), function (req, res) {
+//     console.log(req.file, req.body)
+//     res.send("oldu")
+//  });
+
+// get  for products
+app.get('/', (req, res) => {
+  res.send(data)
+})
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  element =  data.find(item=>item.id == id)
+  res.send(element)
+})
+
+app.post("/",  function (req, res) {
+    const body = req.body;
+    idCounter++
+    body.id = idCounter
+    data.push(body)
+    res.send(body)
+  })
+
+app.put("/:id",(req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    element =  data.find(item=>item.id == id)
+    body.id = element.id
+    data =  data.filter(item=>item.id != id)
+    data.push(body)
+    res.send(body)
+  })
+
+app.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    data =  data.filter(item=>item.id != id)
+    res.send(id)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
